403aac652f91ad4e9fc8fde524dc557b6e17bc62
kaiping <kaiping@kaipingdeMacBook-Air.local>
2026-09-08 21:03:07 +0800

    chore: stop tracking archived files

--- olist封存/Data_Understanding封存/DataGrip_project/olist/stagging/cast/stg_sellers.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS olist_staging.stg_sellers AS
-SELECT *
-FROM olist_raw.sellers;
--- olist封存/資料理解/sql/data_quality/num_logic_check/review_score_check.sql
+++
@@ -1,8 +0,0 @@
-USE olist_staging;
-
-SELECT
-    'stg_reviews' AS table_name,
-    'review_score_not_between_1_and_5' AS check_name,
-    COUNT(*) AS row_count,
-    SUM(review_score < 1 OR review_score > 5) AS issue_count
-FROM stg_reviews;
